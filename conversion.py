2b1b3c10ec3db3cceeb0f43fae2e5d6268a0bfd3
Makereke <makereke1024@gmail.com>
2025-09-19 15:57:23 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,20 @@
+num1 = 99,23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+#converting float to integer data type
+num1_new = int(num1) 
+#converting integer to a float data type
+num2_new = float(num2)
+
+#cvonverting integer to a string
+num3_new = str(num3)
+
+#converting a string to integer
+string1_new = int(string1)
+
+print(num1_new)
+print(num2_new)
+print(num3_new)
+print(string1_new)
